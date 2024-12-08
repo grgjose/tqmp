a5f5e982be1877faa84100953c39a133b481f7cd
George Louis Jose <georgelouisjose@gmail.com>
2024-12-09 02:28:49 +0800

    Added Controller and Views

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -11,4 +11,16 @@ class HomeController extends Controller
     public function index(){
         return view('home.index');
     }
+
+    public function about(){
+        return view('home.about');
+    }
+
+    public function contact(){
+        return view('home.contact');
+    }
+
+    public function services(){
+        return view('home.services');
+    }
 }
