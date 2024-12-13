d58a6d331f12ac6d8ddada444881e5f0dc019715
Sev <genevagarcia0704@gmail.com>
2024-12-14 01:46:47 +0800

    Edited Bulletproofing Page

--- resources/views/bulletproofing/index.blade.php
+++ resources/views/bulletproofing/index.blade.php
@@ -96,6 +96,27 @@
     </section>
     <!-- End of Services -->
 
+    <!-- Trial -->
+    <div class="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center bg-body-tertiary">
+    <div class="col-md-6 p-lg-5 mx-auto my-5">
+      <h1 class="display-3 fw-bold">Designed for engineers</h1>
+      <h3 class="fw-normal text-muted mb-3">Build anything you want with Aperture</h3>
+      <div class="d-flex gap-3 justify-content-center lead fw-normal">
+        <a class="icon-link" href="#">
+          Learn more
+          <svg class="bi"><use xlink:href="#chevron-right"/></svg>
+        </a>
+        <a class="icon-link" href="#">
+          Buy
+          <svg class="bi"><use xlink:href="#chevron-right"/></svg>
+        </a>
+      </div>
+    </div>
+    <div class="product-device shadow-sm d-none d-md-block"></div>
+    <div class="product-device product-device-2 shadow-sm d-none d-md-block"></div>
+  </div>
+    <!-- End of Trial -->
+
     <!-- Bullet Proofing Section -->
     <div class="container py-4">
         <section class="custom-section fade-in">
