dd0bb2e8a6186578b509c07121de6c0c587bc53e
Sev <genevagarcia0704@gmail.com>
2024-12-16 03:15:17 +0800

    Added x-icon

--- resources/views/home/contact.blade.php
+++ resources/views/home/contact.blade.php
@@ -6,6 +6,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Contact Us</title>
+    <link rel="icon" href="{{ asset('storage/logos/TQMPLogo.png') }}" type="image/x-icon">
 
     <!-- Preconnect for External Resources -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
