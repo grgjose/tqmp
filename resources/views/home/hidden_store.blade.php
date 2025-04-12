e36f92d52c8813a3599d55bb343b6911d1717b4a
Sev <genevagarcia0704@gmail.com>
2025-04-13 04:00:40 +0800

    Remove unused JavaScript includes and clean up footer section

--- resources/views/home/hidden_store.blade.php
+++ resources/views/home/hidden_store.blade.php
@@ -179,30 +179,15 @@
         });
     </script>
 
-    <!-- JS for searchbar functionality -->
-
-
     <!-- -----Footers----- -->
-
-    <!-- Accordion Section -->
-    @include ('plus.accordion')
-    <!-- End of Accordion -->
-
-    <!-- Call to Action -->
-    @include ('plus.cta')
-    <!-- End of Call to Action -->
 
     <!-- Footer -->
     @include ('plus.footer')
     <!-- End of Footer -->
 
 
-
-    <!-- Required Popper.js -->
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
-
-    <!-- Bootstrap JavaScript -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
+    <!-- Bootstrap JS -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 
 </html>
