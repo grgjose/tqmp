b12b84b3d702de7e54577e92b6717bbe02a8e461
Sev <genevagarcia0704@gmail.com>
2025-04-26 05:26:56 +0800

    Updated UserProfileController

--- app/Http/Controllers/UserProfileController.php
+++ app/Http/Controllers/UserProfileController.php
@@ -3,6 +3,10 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Quotation;
+use App\Models\QuotationImage;
+use App\Models\QuotationMessage;
+use Illuminate\Support\Facades\DB;
 
 class UserProfileController extends Controller
 {
@@ -62,14 +66,27 @@ class UserProfileController extends Controller
         ]);
     }
 
-    public function user_messages()
+    public function user_messages($reference = '000001-3')
     {
         /** @var \Illuminate\Auth\SessionGuard $auth */
         $auth = auth();
         $my_user = $auth->user();
 
+        $quotation = DB::table('quotations')->where('reference', '=', $reference)->get();
+        if ($quotation == null || count($quotation) == 0) {
+            return redirect('/profile')->with('error_msg', 'Invalid Quotation');
+        }
+
+        $quotationMessages = DB::table('quotation_messages')
+            ->join('users', 'quotation_messages.from_user_id', '=', 'users.id')
+            ->select('quotation_messages.*', 'users.usertype as usertype', 'users.fname as fname', 'users.lname as lname')
+            ->where('quotation_messages.quotation_id', '=', $quotation[0]->id)
+            ->get();
+
         return view('home.user_messages', [
             'my_user' => $my_user,
+            'quotation' => $quotation[0],
+            'quotationMessages' => $quotationMessages,
         ]);
     }
 
