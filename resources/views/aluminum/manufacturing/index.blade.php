d05a45928ebdccba0ac9ebc7299092d86d84583a
Sev <genevagarcia0704@gmail.com>
2025-05-01 03:07:08 +0800

    Updated Hidden Store UI

--- resources/views/aluminum/manufacturing/index.blade.php
+++ resources/views/aluminum/manufacturing/index.blade.php
@@ -395,6 +395,14 @@
     <!-- Footer -->
     @include ('plus.footer')
     <!-- End of Footer -->
+
+    
+    <!-- Scripts -->
+    @include ('plus.scripts')
+    <!-- End of Scripts -->
+
+    {{-- <!-- Bootstrap JS -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script> --}}
 </body>
 
 </html>
